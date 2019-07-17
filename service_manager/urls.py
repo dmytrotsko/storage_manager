6649416cb8f2b2c7ff6a77637f1f5e23b35ccddb
Artur Dankanych <artur.dankanych@synapse.com>
2019-07-17 17:42:41 +0300

    Added reports generations logic and temp template to service manager

--- service_manager/urls.py
+++ service_manager/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import villa_list, villa_expenses
+from .views import villa_list, villa_expenses, villa_reports, generate_update_report
 
 
 urlpatterns = [
     path('', villa_list, name="villas_list"),
-    path("/<int:pk>/expenses", villa_expenses, name='villa_expenses'),
-    path("/<int:pk>/reports", villa_reports, name='villa_reports'),
+    path("<int:pk>/expenses", villa_expenses, name='villa_expenses'),
+    path("<int:villa_id>/reports", villa_reports, name='villa_reports'),
+    path("<int:villa_id>/reports/new_report", generate_update_report, name='new_report'),
+    path("<int:villa_id>/reports/<int:report_id>", generate_update_report, name='edit_report'),
+
 ]
--- service_manager/views.py
+++ service_manager/views.py
@@ -1,7 +1,11 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
 from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils.dateparse import parse_date
 from indussystem.models import Villa
 from .models import VillaReports, Service, ServiceType
 
@@ -43,14 +47,69 @@ def villa_expenses(request, pk):
                                                                     'services_types': services_types})
 
 def villa_reports(request, villa_id):
-    villa = Villa.objaects.get(pk=villa_id)
-    villa_reports_list = VillaReports.objects.get(villa = villa)
+    villa = Villa.objects.get(pk=villa_id)
+    villa_reports_list = VillaReports.objects.filter(villa = villa)
     paginator = Paginator(villa_reports_list, 25)
 
     page = request.GET.get('page')
     reports = paginator.get_page(page)
-    return render(request, "service_manager/villa_reports.html", {'reprots': reports})
+    return render(request, "service_manager/villa_reports.html", {'reports': reports,
+                                                                  'villa': villa})
 
 
-def generate_update_report(request, villa_id, for_custom_period=1,update=0):
-    villa = Villa.objaects.get(pk=villa_id)
+def generate_update_report(request, villa_id,report_id=-1):
+    villa = Villa.objects.get(pk=villa_id)
+    today = datetime.date.today()
+    d = today - relativedelta(months=1)
+    
+    if report_id >= 0:
+        report = VillaReports.objects.get(pk=report_id)
+        start_date = report.start_date
+        end_date = report.end_date
+    else:
+        report = None
+        start_date = datetime.date(d.year, d.month, 1).strftime('%Y-%m-%d')
+        end_date = (datetime.date(today.year, today.month, 1) - relativedelta(days=1)).strftime('%Y-%m-%d')
+
+    if request.method == 'POST':
+        start_date = parse_date(request.POST.get('start_date'))
+        end_date = parse_date(request.POST.get('end_date'))
+        services_list = Service.objects.filter(villa=villa).filter(date__range = [start_date, end_date])
+        expenses = 0
+        for service in services_list:
+            expenses += service.price
+        
+        income = float(request.POST.get('income'))
+        occupancy = int(request.POST.get('occupancy'))
+        profit = income - expenses
+        averange_price = income/occupancy
+        if report:
+            report.start_date = start_date
+            report.end_date = end_date
+            report.expenses = expenses
+            report.income = income
+            report.occupancy = occupancy
+            report.profit = profit
+            report.averange_price = averange_price
+            
+        else:
+            report = VillaReports.objects.create(villa=villa,
+                                        income=income,
+                                        expenses=expenses,
+                                        occupancy=occupancy,
+                                        profit=profit,
+                                        averange_price=averange_price,
+                                        start_date=start_date,
+                                        end_date=end_date )
+        report.save()
+        
+        return HttpResponseRedirect("/service_manager/{}/reports".format(villa_id))
+
+    ctx = {'villa': villa,
+           'report': report,
+           'start_date':start_date,
+           'end_date':end_date}
+
+    return render(request, 'service_manager/villa_financial_report.html', ctx)
+
+
